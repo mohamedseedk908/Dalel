eabc859bdc8593357881fb74cbff772260e34958
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-03-19 19:06:00 +0200

    create name app

---
+++ android/app/src/main/kotlin/com/dalel/dalel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dalel.dalel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'dalel_app.dart';
+
+void main(){
+  runApp(Dalel());
+}
